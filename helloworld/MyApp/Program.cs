3e1e03c49f87e735e1c4e5ad412f6710149aaec0
Johan Prianto Manik Ambarita <johan.prianto.manik.a@brilian.bri.co.id>
2023-03-16 10:30:14 +0700

    up

---
+++ helloworld/MyApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=0;
+            for(i=0; i<10;i++){
+                Console.WriteLine("Hello World!");
+            }
+
+            if(i < 10){
+                Console.WriteLine("Hello World!" + i);
+            }else if(i == 10)
+            {
+                Console.WriteLine("Hello World!" + i);
+            }
+            else
+            {
+                Console.WriteLine("Hello World!" + i);
+            }
+
+        }
+    }
+}
